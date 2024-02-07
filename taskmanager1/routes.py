0d4f2c86f7e8a87879da6d038235be1dc0969617
Elvira Tidemo <elvir@MacBook-Air.local>
2024-02-07 10:30:42 +0100

    Add categories and remove venv

--- taskmanager1/routes.py
+++ taskmanager1/routes.py
@@ -5,3 +5,11 @@ from taskmanager1 import app, db
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+@app.route("/add_category", method=["GET", "POST"])
+def add_category():
+    return render_template("add_category.html")
